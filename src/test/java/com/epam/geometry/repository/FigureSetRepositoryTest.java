5e4ac0733c1590d14bde36bde31f97180b277461
geobreze <geobreze@gmail.com>
2019-02-16 17:20:00 +0300

    new test for query with comparator

--- src/test/java/com/epam/geometry/repository/FigureSetRepositoryTest.java
+++ src/test/java/com/epam/geometry/repository/FigureSetRepositoryTest.java
@@ -9,6 +9,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 import static org.mockito.Mockito.*;
@@ -27,18 +28,36 @@ public class FigureSetRepositoryTest {
             new Point2D(1, 2),
             new Point2D(1, -1),
             new Point2D(-2, -1));
-    private final FigureRepository repository = new FigureSetRepository();
 
     @Test
     public void queryShouldReturnSquareWhenSpecificationSupplied() {
         FigureSpecification specification = mock(FigureSpecification.class);
         when(specification.specified(TEST_SQUARE)).thenReturn(true);
         when(specification.specified(TEST_TRAPEZIUM)).thenReturn(false);
+        FigureRepository repository = new FigureSetRepository();
         repository.addFigure(TEST_TRAPEZIUM);
         repository.addFigure(TEST_SQUARE);
 
         List<Figure> expected = Arrays.asList(TEST_SQUARE);
         List<Figure> actual = repository.query(specification);
+
+        Assert.assertEquals(expected, actual);
+        verify(specification, times(2)).specified(any(Figure.class));
+        verifyNoMoreInteractions(specification);
+    }
+
+    @Test
+    public void getSortedShouldSortOrderedByIdFigures() {
+        Comparator<Figure> comparator = mock(Comparator.class);
+        FigureSpecification specification = mock(FigureSpecification.class);
+        when(specification.specified(any(Figure.class))).thenReturn(true);
+        when(comparator.compare(TEST_SQUARE, TEST_TRAPEZIUM)).thenReturn(1);
+        FigureRepository repository = new FigureSetRepository();
+        repository.addFigure(TEST_TRAPEZIUM);
+        repository.addFigure(TEST_SQUARE);
+
+        List<Figure> expected = Arrays.asList(TEST_SQUARE, TEST_TRAPEZIUM);
+        List<Figure> actual = repository.query(specification, comparator);
 
         Assert.assertEquals(expected, actual);
         verify(specification, times(2)).specified(any(Figure.class));
